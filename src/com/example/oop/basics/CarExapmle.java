94896312d04fcb1febbf34404ce8d61012281412
Wiktor Andrzejkiewicz <wiktor.andrzejkiewicz@gmail.com>
2023-02-07 23:17:00 +0100

    konstruktory

--- src/com/example/oop/basics/CarExapmle.java
+++ src/com/example/oop/basics/CarExapmle.java
@@ -7,6 +7,20 @@ class Car {
     String color;
     float topSpeed;
 
+    public Car(String manufacturer, String name, int year, String color, float topSpeed) {
+      this.manufacturer = manufacturer;
+      this.name = name;
+      this.year = year;
+      this.color = color;
+      this.topSpeed = topSpeed;
+    }
+    public Car () {
+        this.manufacturer = "unknown";
+    }
+
+    public void printInfo() {
+        System.out.println(this.manufacturer + " " + this.name + " " + this.year);
+    }
 }
 public class CarExapmle {
     public static void main(String[] args) {
@@ -16,9 +30,15 @@ public class CarExapmle {
         ford.color = "Blue";
         ford.year = 1967;
         ford.topSpeed = 200.0f;
+        ford.printInfo();
 
         Car chevrolet = new Car();
         chevrolet.manufacturer = "Chevrolet";
+        chevrolet.name = "Camaro";
+        chevrolet.year = 2020;
+        chevrolet.topSpeed = 300.00f;
+        chevrolet.printInfo();
 
+        Car dodge = new Car ("Dodge", "Viper", 1997, "blue", 270.0f);
     }
 }
