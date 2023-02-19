86fd04637e684e8d1e739beac6638ffaa1032838
Wiktor Andrzejkiewicz <wiktor.andrzejkiewicz@gmail.com>
2023-02-19 23:57:01 +0100

    ThisUsage

--- src/com/example/basics/Main.java
+++ src/com/example/basics/Main.java
@@ -3,7 +3,7 @@ package com.example.basics;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world");
+        System.out.println((1+1)+1+"1"+1+1);
 //        System.out.println("Hello again");
         /* koment
         konent
