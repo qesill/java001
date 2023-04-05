8f5361390869d0d7d92e9b3492a98b3f61975aa0
Wiktor Andrzejkiewicz <wiktor.andrzejkiewicz@gmail.com>
2023-04-05 23:07:42 +0200

    ćwiczenie: "interfejsy - rozszerzanie"

---
+++ src/com/example/oop/basics/interface_extends_default_static_final_methods/Example.java
@@ -0,0 +1,11 @@
+package com.example.oop.basics.interface_extends_default_static_final_methods;
+
+public class Example {
+    public static void main(String[] args) {
+Parrot parrot = new Parrot();
+parrot.fly();
+parrot.searchForFood();
+        System.out.println(parrot.getWingspan());
+        System.out.println("Default number of wings: " + Flying.getDefoultNumWings());
+    }
+}
