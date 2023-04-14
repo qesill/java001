b3ae4c40e48322e1c1ab90efe3d2e85bab021c85
Wiktor Andrzejkiewicz <wiktor.andrzejkiewicz@gmail.com>
2023-04-14 06:06:40 +0200

    ćwiczenie: "HashMapBasics"

---
+++ src/com/example/collections/HashMapBasics.java
@@ -0,0 +1,55 @@
+package com.example.collections;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+class Employee {
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Employee(String name) {
+        this.name = name;
+    }
+}
+public class HashMapBasics {
+    public static void main(String[] args) {
+        HashMap<String, Employee> map = new HashMap<>();
+        map.put("Adam", new Employee("Adam"));
+        map.put("Kasia", new Employee("Kasia"));
+        map.put("Ola", new Employee("Ola"));
+
+        Employee e = map.get("Kasia");
+        System.out.println(e); //Kasia
+
+        Iterator i = map.entrySet().iterator();
+        while (i.hasNext()) {
+            Map.Entry entry = (Map.Entry) i.next();
+            String key = (String)entry.getKey();
+            Employee employee = (Employee) entry.getValue();
+            System.out.println(key + " - " + employee);
+        }
+
+        System.out.println("\n");
+
+        for (Map.Entry<String, Employee> entry: map.entrySet()) {
+            String key = entry.getKey();
+            Employee employee = entry.getValue();
+            System.out.println(key + " - " + employee);
+        }
+    }
+}
