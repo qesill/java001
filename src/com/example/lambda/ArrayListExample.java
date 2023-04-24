4b4b931ac74cc4f5af36a251c66185d820cd1d79
Wiktor Andrzejkiewicz <wiktor.andrzejkiewicz@gmail.com>
2023-04-24 23:43:40 +0200

    ćwiczenie: "lambda"

---
+++ src/com/example/lambda/ArrayListExample.java
@@ -0,0 +1,20 @@
+package com.example.lambda;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("bmw");
+        arr.add("merc");
+        arr.add(null);
+        arr.add("pontiac");
+        arr.add(null);
+
+        arr.removeIf(a -> a == null || a.equalsIgnoreCase("merc"));
+
+        System.out.println(arr);
+    }
+}
