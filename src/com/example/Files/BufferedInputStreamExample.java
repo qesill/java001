c266c471b9c1d28ad19071512c50b6c678ceb72a
Wiktor Andrzejkiewicz <wiktor.andrzejkiewicz@gmail.com>
2023-05-09 00:12:19 +0200

    ćwiczenie: "OutputStream"

---
+++ src/com/example/Files/BufferedInputStreamExample.java
@@ -0,0 +1,28 @@
+package com.example.Files;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class BufferedInputStreamExample {
+    public static void main(String[] args)
+            throws IOException {
+
+        BufferedInputStream in = null;
+
+        try {
+            in = new BufferedInputStream(
+                    new FileInputStream("test.txt")
+            );
+
+            int v = 0;
+            while((v = in.read()) != -1) {
+                System.out.println((char)v);
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        } finally {
+            if (in != null) in.close();
+        }
+    }
+}
